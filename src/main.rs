3ca21a08b23ff9f78c2a57c19c0f75374e9de443
tedbauer <tjb272@cornell.edu>
2024-06-15 10:39:02 -0400

    Cycle through stops configured in yaml

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use chrono::{NaiveDateTime, Timelike};
 use serde::Deserialize;
-use std::env;
+use std::{env, fs};
 use tokio::spawn;
 use tokio::time::interval;
 use tokio::time::Duration;
@@ -23,6 +23,17 @@ struct TrainInfo {
     eta: Vec<EtaInfo>,
 }
 
+#[derive(Debug, Deserialize, Clone)]
+struct Stop {
+    name: String,
+    id: usize,
+}
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    stops: Vec<Stop>,
+}
+
 fn build_request_string(api_key: &str, stop_id: usize) -> String {
     format!("{BASE_REQUEST_STRING}?stpid={stop_id}&key={api_key}")
 }
@@ -33,32 +44,30 @@ fn format_time(time_str: &str) -> Result<String, String> {
     Ok(format!("{}:{}", date_time.hour(), date_time.minute()))
 }
 
-fn generate_display_text(train_info: &TrainInfo, stop_id: usize) -> String {
-    let stop = &train_info
+fn generate_display_text(train_info: &TrainInfo, stop: &Stop) -> String {
+    let train_info_stop = &train_info
         .eta
         .first()
         .map(|info| info.stop_description.clone());
+    
+    let stop_name = &stop.name;
 
-    let times = stop.as_ref().map(|_| {
-        train_info
-            .eta
-            .iter()
-            .map(|e| format_time(&e.arrival_time).unwrap())
-            .collect::<Vec<_>>()
-            .join(" ")
-    });
-
-    match (stop, times) {
-        (Some(s), Some(t)) => {
-            format!("{s}\n--\n{t}\n")
-        }
-        _ => format!("No trains scheduled for {stop_id}"),
-    }
+    train_info_stop.as_ref()
+        .map(|s| {
+            let times = train_info
+                .eta
+                .iter()
+                .map(|e| format_time(&e.arrival_time).unwrap())
+                .collect::<Vec<_>>()
+                .join(" ");
+            format!("{stop_name}\n{s}\n--\n{times}\n")
+        })
+        .unwrap_or(format!("{stop_name}\n\n--\nNo trains scheduled"))
 }
 
-async fn try_cycle_display() -> Result<(), String> {
+async fn try_cycle_display(stop: Stop) -> Result<(), String> {
     let api_key = env::var("TRAIN_API_KEY").expect("need api key");
-    let response = reqwest::get(build_request_string(&api_key, 30173))
+    let response = reqwest::get(build_request_string(&api_key, stop.id))
         .await
         .map_err(|err| format!("failed to make get request: {err}"))?;
 
@@ -70,7 +79,7 @@ async fn try_cycle_display() -> Result<(), String> {
         let train_info: TrainInfo = serde_xml_rs::from_str(&body)
             .map_err(|err| format!("failed to serialize response: {err}"))?;
 
-        let display = generate_display_text(&train_info, 30173);
+        let display = generate_display_text(&train_info, &stop);
         println!("{display}");
 
         Ok(())
@@ -86,8 +95,22 @@ async fn try_cycle_display() -> Result<(), String> {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut interval = interval(Duration::from_secs(10));
 
+    let stop_yaml = "stops.yaml";
+    let config: Config = serde_yaml::from_str(
+        &fs::read_to_string(stop_yaml).map_err(|err| format!("failed to load yaml: {err}"))?,
+    )
+    .map_err(|err| format!("failed to parse yaml: {err}"))?;
+
+    let mut current_stop_index = 0;
     loop {
         interval.tick().await;
-        spawn(async { try_cycle_display().await });
+
+        let stop = config
+            .stops
+            .get(current_stop_index)
+            .ok_or(format!("invalid config index: {current_stop_index}"))?.clone();
+        spawn(async { try_cycle_display(stop).await });
+
+        current_stop_index = (current_stop_index + 1) % config.stops.len();
     }
 }
